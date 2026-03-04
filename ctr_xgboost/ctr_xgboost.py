22a250f95efcf564b3cf48c412e455b560c81314
977ADAM <977team.95@gmail.com>
2026-03-04 17:33:26 +0300

    up

--- ctr_xgboost/ctr_xgboost.py
+++ ctr_xgboost/ctr_xgboost.py
@@ -2,12 +2,13 @@ import pandas as pd
 import numpy as np
 import joblib
 
-from sklearn.model_selection import train_test_split
+from sklearn.model_selection import train_test_split, KFold
 from sklearn.pipeline import Pipeline
 from sklearn.compose import ColumnTransformer
 from sklearn.impute import SimpleImputer
-from sklearn.preprocessing import OneHotEncoder
 from sklearn.metrics import roc_auc_score
+
+from category_encoders import CatBoostEncoder, TargetEncoder
 
 from xgboost import XGBClassifier
 
@@ -17,34 +18,59 @@ df = pd.read_csv("./data/dataset_train.csv")
 df_test = pd.read_csv("./data/dataset_test.csv")
 
 
-df["DateTime"] = pd.to_datetime(df["DateTime"])
-dt = df["DateTime"]
-df["year"] = dt.dt.year
-df["month"] = dt.dt.month
-df["day"] = dt.dt.day
-df["dayofweek"] = dt.dt.dayofweek
-df["hour"] = dt.dt.hour
-df["weekofyear"] = dt.dt.isocalendar().week.astype(np.int16)
-df["is_weekend"] = df["dayofweek"].isin([5, 6]).astype(np.int8)
-df["hour_sin"] = np.sin(2 * np.pi * df["hour"] / 24)
-df["hour_cos"] = np.cos(2 * np.pi * df["hour"] / 24)
-df["dow_sin"] = np.sin(2 * np.pi * df["dayofweek"] / 7)
-df["dow_cos"] = np.cos(2 * np.pi * df["dayofweek"] / 7)
+def oof_ctr_feature(df, col, target, n_splits=5):
+
+    kf = KFold(n_splits=n_splits, shuffle=True, random_state=42)
+
+    oof = np.zeros(len(df))
+
+    global_ctr = df[target].mean()
+
+    for train_idx, valid_idx in kf.split(df):
+
+        train_fold = df.iloc[train_idx]
+        valid_fold = df.iloc[valid_idx]
+
+        ctr = train_fold.groupby(col)[target].mean()
+
+        oof[valid_idx] = valid_fold[col].map(ctr)
+
+    oof = pd.Series(oof, index=df.index)
+
+    oof.fillna(global_ctr, inplace=True)
+
+    return oof
 
 
-df_test["DateTime"] = pd.to_datetime(df_test["DateTime"])
-dt = df_test["DateTime"]
-df_test["year"] = dt.dt.year
-df_test["month"] = dt.dt.month
-df_test["day"] = dt.dt.day
-df_test["dayofweek"] = dt.dt.dayofweek
-df_test["hour"] = dt.dt.hour
-df_test["weekofyear"] = dt.dt.isocalendar().week.astype(np.int16)
-df_test["is_weekend"] = df_test["dayofweek"].isin([5,6]).astype(np.int8)
-df_test["hour_sin"] = np.sin(2*np.pi*df_test["hour"]/24)
-df_test["hour_cos"] = np.cos(2*np.pi*df_test["hour"]/24)
-df_test["dow_sin"] = np.sin(2*np.pi*df_test["dayofweek"]/7)
-df_test["dow_cos"] = np.cos(2*np.pi*df_test["dayofweek"]/7)
+def add_time_features(df: pd.DataFrame) -> pd.DataFrame:
+    df = df.copy()
+
+    df["DateTime"] = pd.to_datetime(df["DateTime"])
+    df = df.sort_values("DateTime")
+    dt = df["DateTime"]
+
+    df["year"] = dt.dt.year
+    df["month"] = dt.dt.month
+    df["day"] = dt.dt.day
+    df["dayofweek"] = dt.dt.dayofweek
+    df["hour"] = dt.dt.hour
+    df["weekofyear"] = dt.dt.isocalendar().week.astype("int16")
+
+    df["is_weekend"] = df["dayofweek"].isin([5,6]).astype("int8")
+
+    df["hour_sin"] = np.sin(2*np.pi*df["hour"]/24)
+    df["hour_cos"] = np.cos(2*np.pi*df["hour"]/24)
+
+    df["dow_sin"] = np.sin(2*np.pi*df["dayofweek"]/7)
+    df["dow_cos"] = np.cos(2*np.pi*df["dayofweek"]/7)
+
+    return df
+
+
+
+df = add_time_features(df)
+df_test = add_time_features(df_test)
+
 
 df_test = df_test.drop(columns=["session_id","DateTime"])
 
@@ -58,6 +84,72 @@ X_train, X_valid, y_train, y_valid = train_test_split(
     stratify=y,
     random_state=42
 )
+
+train_df = X_train.copy()
+train_df["is_click"] = y_train
+
+X_train["user_ctr"] = oof_ctr_feature(
+    train_df,
+    col="user_id",
+    target="is_click"
+)
+
+X_train["product_ctr"] = oof_ctr_feature(
+    train_df,
+    col="product",
+    target="is_click"
+)
+train_df["user_product"] = (
+    train_df["user_id"].astype(str)
+    + "_"
+    + train_df["product"].astype(str)
+)
+
+X_train["user_product_ctr"] = oof_ctr_feature(
+    train_df,
+    col="user_product",
+    target="is_click"
+)
+
+user_ctr = train_df.groupby("user_id")["is_click"].mean()
+
+product_ctr = train_df.groupby("product")["is_click"].mean()
+
+user_product_ctr = train_df.groupby("user_product")["is_click"].mean()
+
+
+X_valid["user_ctr"] = X_valid["user_id"].map(user_ctr)
+
+X_valid["product_ctr"] = X_valid["product"].map(product_ctr)
+
+X_valid["user_product"] = (
+    X_valid["user_id"].astype(str)
+    + "_"
+    + X_valid["product"].astype(str)
+)
+
+X_valid["user_product_ctr"] = X_valid["user_product"].map(user_product_ctr)
+
+# =================================================================================================
+
+df_test["user_ctr"] = df_test["user_id"].map(user_ctr)
+
+df_test["product_ctr"] = df_test["product"].map(product_ctr)
+
+df_test["user_product"] = (
+    df_test["user_id"].astype(str)
+    + "_"
+    + df_test["product"].astype(str)
+)
+
+df_test["user_product_ctr"] = df_test["user_product"].map(user_product_ctr)
+
+fill_value = y_train.mean()
+
+for col in ["user_ctr", "product_ctr", "user_product_ctr"]:
+    X_train[col].fillna(fill_value, inplace=True)
+    X_valid[col].fillna(fill_value, inplace=True)
+    df_test[col].fillna(fill_value, inplace=True)
 
 numeric_features = [
     "age_level",
@@ -75,9 +167,15 @@ numeric_features = [
     'hour_cos',
     'dow_sin',
     'dow_cos',
+    "user_ctr",
+    "product_ctr",
+    "user_product_ctr"
 ]
 
 categorical_features = [
+    "user_id",
+    "campaign_id",
+    "webpage_id",
     "product",
     "product_category_1",
     "product_category_2",
@@ -93,7 +191,7 @@ numeric_pipeline = Pipeline([
 
 categorical_pipeline = Pipeline([
     ("imputer", SimpleImputer(strategy="most_frequent")),
-    ("encoder", OneHotEncoder(handle_unknown="ignore"))
+    ("encoder",  TargetEncoder(smoothing=10))
 ])
 
 preprocessor = ColumnTransformer(
@@ -103,18 +201,18 @@ preprocessor = ColumnTransformer(
     ]
 )
 
-scale_pos_weight = (y == 0).sum() / (y == 1).sum()
+scale_pos_weight = (y_train == 0).sum() / (y_train == 1).sum()
 
 model = XGBClassifier(
-    n_estimators=500,
+    n_estimators=300,
     max_depth=6,
-    learning_rate=0.05,
+    learning_rate=0.01,
     subsample=0.8,
     colsample_bytree=0.8,
-    eval_metric="logloss",
-    tree_method="hist",
+    eval_metric="auc",
+    tree_method="auto",
     scale_pos_weight=scale_pos_weight,
-    random_state=42
+    random_state=42,
 )
 
 pipeline = Pipeline([
